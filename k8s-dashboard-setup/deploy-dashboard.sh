2570ea9ce1c1df8078c69b7971b650f6cd8a92eb
Hirdesh Kumar Yadav <125953445+iamhirdeshkumar15@users.noreply.github.com>
2025-03-26 09:14:59 +0530

    Add automation script for Kubernetes Dashboard setup

--- k8s-dashboard-setup/deploy-dashboard.sh
+++ k8s-dashboard-setup/deploy-dashboard.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# Deploy the Kubernetes Dashboard
+echo "Deploying Kubernetes Dashboard..."
+
+# Deploy the official Kubernetes Dashboard
 kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
-# Modify the Dashboard service to use NodePort
+echo "Modifying Kubernetes Dashboard service to use NodePort..."
+
+# Change the service type from ClusterIP to NodePort
 kubectl patch svc kubernetes-dashboard -n kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
 
 # Get the assigned NodePort
-kubectl get svc kubernetes-dashboard -n kubernetes-dashboard
+NODE_PORT=$(kubectl get svc kubernetes-dashboard -n kubernetes-dashboard -o jsonpath="{.spec.ports[0].nodePort}")
+echo "Dashboard is accessible at https://<NODE_IP>:${NODE_PORT}"
 
 # Create an admin service account
+echo "Creating admin service account for the Dashboard..."
 kubectl create serviceaccount cluster-admin-dashboard-sa -n kubernetes-dashboard
 
 # Grant cluster-admin role to the service account
+echo "Assigning cluster-admin privileges..."
 kubectl create clusterrolebinding cluster-admin-dashboard-sa   --clusterrole=cluster-admin   --serviceaccount=kubernetes-dashboard:cluster-admin-dashboard-sa
 
-# Get and display the login token
+# Generate and display the login token
+echo "Fetching authentication token for Kubernetes Dashboard login..."
 kubectl -n kubernetes-dashboard create token cluster-admin-dashboard-sa
 
-echo "Kubernetes Dashboard is deployed! Access it using: https://<NODE_IP>:<NODE_PORT>"
+echo "Kubernetes Dashboard has been successfully deployed! Use the token above to log in."
